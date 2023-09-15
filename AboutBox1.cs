19d519f10301c43e536c66319f3a1649f7e17099
Yegor Stepkin <29415403@s.mfua.ru>
2023-09-15 16:20:06 +0300

    ok fixed

--- AboutBox1.cs
+++ AboutBox1.cs
@@ -101,5 +101,10 @@ namespace PictureViewerStepkin03ПРд4310
             }
         }
         #endregion
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
